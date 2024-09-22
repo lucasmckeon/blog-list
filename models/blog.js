73de809e954bd186397886afa8eb4f9caf808ccc
lucasmckeon <95000203+lucasmckeon@users.noreply.github.com>
2024-09-22 11:54:21 -1000

    Replace Blog _id property with id, as id is more readable than _id. Test id property present. Verity HTTP POST request to the /api/blogs URL successfully creates a new blog post

--- models/blog.js
+++ models/blog.js
@@ -1,11 +1,23 @@
 import mongoose from 'mongoose';
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+  },
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
 const Blog = mongoose.model('Blog', blogSchema);
 export { Blog };
--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -19,16 +19,40 @@ beforeEach(async () => {
 
 const api = supertest(app);
 
-test.only('blog list application returns the correct amount of blog posts in the JSON format', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-  const initialBlogCount = getInitialAmountOfBlogs();
-  assert(initialBlogCount === response.body.length);
+describe('when blog posts retrieved', () => {
+  test('blog list application returns the correct amount of blog posts in the JSON format', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+    const initialBlogCount = getInitialAmountOfBlogs();
+    assert(initialBlogCount === response.body.length);
+  });
+
+  test('unique identifier property of the blog posts is named id', async () => {
+    const blogs = await getBlogsInDB();
+    blogs.forEach((blog) => {
+      // @ts-ignore
+      assert(blog.id && !blog._id);
+    });
+  });
 });
 
-describe('verifying like utility methods', () => {
+test.only('POST request creates a new blog post', async () => {
+  const title = 'Test Title';
+  const newBlog = { title, author: 'Test Author', url: 'Test URL', likes: 12 };
+  const blog = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+  const blogs = await getBlogsInDB();
+  assert(blogs.length === getInitialAmountOfBlogs() + 1);
+  const titles = blogs.map((b) => b.title);
+  assert(titles.includes(title));
+});
+
+describe('like utility methods', () => {
   test('total likes is 36', async () => {
     const blogs = await getBlogsInDB();
     assert.strictEqual(totalLikes(blogs), 36);
